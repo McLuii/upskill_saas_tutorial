bedeff0d3a25300563946b131ce5cd1395dcdc7e
Liam McNie <liam.mcnie@gmail.com>
2022-07-20 18:08:02 +0000

    Added stripe token to users table

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,6 @@
+require 'stripe'
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
+
+#Stripe::Product.create({name: 'Basic Plan', id: '1'})
+#Stripe::Product.create({name: 'Pro Plan', id: '2'})
