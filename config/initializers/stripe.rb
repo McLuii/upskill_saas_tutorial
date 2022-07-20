0062a1dbc76320e43906fe98f6d1252afe49a9de
Liam McNie <liam.mcnie@gmail.com>
2022-07-20 19:13:50 +0000

    Resolved errors

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,5 +2,3 @@ require 'stripe'
 Stripe.api_key = ENV["stripe_api_key"]
 STRIPE_PUBLIC_KEY = ENV["stripe_publishable_key"]
 
-#Stripe::Product.create({name: 'Basic Plan', id: '1'})
-#Stripe::Product.create({name: 'Pro Plan', id: '2'})
