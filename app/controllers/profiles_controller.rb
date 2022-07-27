4211247059ab3944516e54765fa4c2f8a8328880
Liam McNie <liam.mcnie@gmail.com>
2022-07-27 02:30:34 +0000

    Added before filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
   # GET to /users/:user_id/profile/new
   def new
     # Render blank profile form
@@ -40,10 +43,15 @@ class ProfilesController < ApplicationController
     end
   end
   def index
-    @users = User.all
+    #@users = User.all
   end
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
+    
+    def only_current_user
+      @user = User.find( params[:user_id] )
+      redirect_to(root_url) unless @user == current_user
     end
 end
